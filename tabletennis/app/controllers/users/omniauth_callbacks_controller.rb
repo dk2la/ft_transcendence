49710edbd7364ed2370776a111821c71f7756b60
dk2la <sjakku@student.21-school.ru>
2021-04-24 16:54:37 +0300

    remove bootstrap

--- tabletennis/app/controllers/users/omniauth_callbacks_controller.rb
+++ tabletennis/app/controllers/users/omniauth_callbacks_controller.rb
@@ -16,7 +16,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def after_sign_up_path_for(resource)
-    signed_in_root_path('/home/index')
+    "http://www.google.com"
   end
 
   def after_omniauth_failure_path_for scope
--- tabletennis/config/routes.rb
+++ tabletennis/config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  root 'start_page#index', to: redirect('/home/index')
-  get 'guilds/index'
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  root 'start_page#index'
+  get '/home/index', as: 'start_page_root'
 
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  
   devise_scope :user do
     delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session_path
   end
-
+  
   get 'users/sign_up', as: 'registration'
   get 'home/index', as: 'home'
   get 'guilds/index', as: 'guild'
   get '/users/sign_in', as: 'login'
-
+  
   resources :guilds
   get '/guilds/new', to: 'guilds#new', as: "new_guildd"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
