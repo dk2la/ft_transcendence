0b47bc592459dbc7229bd98e2989344cc2777a47
dk2la <sjakku@student.21-school.ru>
2021-04-27 14:46:04 +0300

    add _form fo edit user, need fix reload page

--- tabletennis/app/controllers/home_controller.rb
+++ tabletennis/app/controllers/home_controller.rb
@@ -16,11 +16,15 @@ class HomeController < ApplicationController
     respond_to do |format|
       if current_user.update(user_params)
         format.html { redirect_to current_user, notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: current_user }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: current_user.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email)
   end
 end
--- tabletennis/config/routes.rb
+++ tabletennis/config/routes.rb
@@ -12,9 +12,8 @@ Rails.application.routes.draw do
   
   resources :home
   get 'home/index', to: 'home#index'
-  get 'home/edit', to: 'home#edit'
-  get 'home/update', as: 'update_user'
   get 'home/:id/edit', to: 'home#edit', as: 'user'
+  patch 'home/:id', to: 'home#update', as: 'update_user'
   
   resources :guilds
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
