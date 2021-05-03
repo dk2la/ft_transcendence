8789b9470804358eb584673d198220acb8e5fb77
Antwan Tomatoes <atomatoe@ge-h4.kzn.21-school.ru>
2021-05-03 20:08:54 +0300

    add new front for gulld, edit forms, list friends and something else

--- tabletennis/app/controllers/list_players_controller.rb
+++ tabletennis/app/controllers/list_players_controller.rb
@@ -1,5 +1,6 @@
 class ListPlayersController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_user, only: [:show]
 
   def index
     @list = User.all
@@ -7,6 +8,12 @@ class ListPlayersController < ApplicationController
 
   def show
     p params
+    @friends = @list.friends
+  end
+
+  private
+
+  def find_user
     @list = User.find(params[:id])
   end
 end
