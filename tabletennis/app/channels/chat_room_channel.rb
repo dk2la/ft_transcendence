742cd6598646b8d245d352e244fa244552accd97
dk2la <sjakku@student.21-school.ru>
2021-05-22 15:07:53 +0300

    remove conflicts

--- tabletennis/app/channels/chat_room_channel.rb
+++ tabletennis/app/channels/chat_room_channel.rb
@@ -1,10 +1,6 @@
 class ChatRoomChannel < ApplicationCable::Channel
   def subscribed
-<<<<<<< HEAD
-    stream_from "chat_room_channel_#{params[:chat_room_id]}"
-=======
     stream_for current_user
->>>>>>> 6d5749fbb98bbae5bd1b452f7a3e0b69667421ed
   end
 
   def unsubscribed
